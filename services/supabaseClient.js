c884071e12e50b9762f800484708451bfd4c5223
Iteam786 <iteamcalender786@gmail.com>
2025-07-21 12:54:44 +0200

    ✅ Fix export supabase client

--- services/supabaseClient.js
+++ services/supabaseClient.js
@@ -1,6 +1,7 @@
+// 📁 backend/services/supabaseClient.js
 import { createClient } from "@supabase/supabase-js";
 import dotenv from "dotenv";
 dotenv.config();
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
-export default supabase;
+export { supabase };
